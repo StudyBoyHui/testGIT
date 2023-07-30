8b6cea382eb8e9bfc4aeb90d09d0229f6d6c026d
StudyBoy <1303283106@qq.com>
2023-07-30 11:55:14 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# 牛马灰灰的python学习
+print("helloWorld")
+print("hello")
+print("Chris Paul")
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+for i in tqdm(range(50)):
+    p = i * 1
+
+print("complete")
+
+fig = plt.figure()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set(xlim=[0.5, 4.5], ylim=[-2, 8], title='An Example Axes',
+       ylabel='Y-Axis', xlabel='X-Axis')
+plt.show()
+
+print("hellow")
